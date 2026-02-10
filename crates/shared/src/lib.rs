681673e547c7e6353d85c1fa72e98e6a7e1662b6
Marcin Szymczak <mail@marszy.com>
2026-02-10 14:45:32 +0100

    Simulator crate

--- crates/shared/src/lib.rs
+++ crates/shared/src/lib.rs
@@ -11,6 +11,7 @@ pub mod bytes;
 pub mod code_fetching;
 pub mod current_block;
 pub mod db_order_conversions;
+pub mod domain;
 pub mod encoded_settlement;
 pub mod event_handling;
 pub mod event_storing_helpers;
